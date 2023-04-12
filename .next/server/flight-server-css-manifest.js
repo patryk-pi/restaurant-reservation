fd957e146a4dc6f49d56245f3adbe73a2f3ca699
patryk-pi <patryk.piasecki1@gmail.com>
2023-04-12 17:33:19 +0200

    basic project setup

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,15 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/patryk/repositories/restaurant-reservation/app/page.tsx": [
+      "/Users/patryk/repositories/restaurant-reservation/node_modules/next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/patryk/repositories/restaurant-reservation/app/layout.tsx": [
+      "/Users/patryk/repositories/restaurant-reservation/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/patryk/repositories/restaurant-reservation/app/page": [
+      "/Users/patryk/repositories/restaurant-reservation/app/globals.css"
+    ]
+  }
+}
